132c763f3ab018d434d2a767da756e49cf76a870
Jonathan Torres <rickijtr@hotmail.com>
2024-11-04 00:02:32 -0600

    Nuevo

---
+++ Class/CarsLoad.js
@@ -0,0 +1,13 @@
+import {OBJLoader} from "../Loaders/OBJLoader.js";
+import { OrbitControls } from "./OrbitControls.js";
+import {MTLLoader} from "../Loaders/MTLLoader.js";
+class CarsSelect{
+
+ // Cargar el modelo OBJ
+
+ 
+
+}
+
+
+export {CarsSelect};
---
+++ js/JavaScript.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const createGameBtn = document.getElementById("create-game-btn");
+    const configuracionesBtn = document.getElementById("configuraciones-btn");
+    const puntuacionesBtn = document.getElementById("puntuaciones-btn");
+    //GameScreen
+    const MenuPausaBtn = document.getElementById("menu-pausa-btn");
+
+    const body = document.body;
+    
+    createGameBtn.addEventListener("click", () => {
+      // Change the page
+    window.location.href = "Escenario1.html";
+    
+      // Add the transition class
+    body.classList.add("transition");
+    
+      // After the transition finishes, remove the class
+    setTimeout(() => {
+        body.classList.remove("transition");
+    }, 500);
+    });
+
+    configuracionesBtn.addEventListener("click", () => {
+        // Change the page
+    window.location.href = "configuraciones.html";
+    
+        // Add the transition class
+    body.classList.add("transition");
+    
+        // After the transition finishes, remove the class
+    setTimeout(() => {
+    body.classList.remove("transition");
+    }, 500);
+    });
+
+    puntuacionesBtn.addEventListener("click", () => {
+        // Change the page
+    window.location.href = "puntuaciones.html";
+    
+        // Add the transition class
+    body.classList.add("transition");
+    
+        // After the transition finishes, remove the class
+    setTimeout(() => {
+    body.classList.remove("transition");
+    }, 500);
+    });
+
+    });
