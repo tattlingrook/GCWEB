fd1543da6e855f5f600e545653cc240cdac1a1e1
Jonathan Torres <rickijtr@hotmail.com>
2024-11-07 01:32:27 -0600

    menu

--- Class/CarsLoad.js
+++ Class/CarsLoad.js
@@ -1,172 +1,51 @@
-import {OBJLoader} from "../Loaders/OBJLoader.js";
-import {MTLLoader} from "../Loaders/MTLLoader.js";
-//import { OrbitControls } from "./OrbitControls.js";
-import * as THREE from "../three.module.js";
+import * as THREE from '../three.module.js';
+import { MTLLoader } from '../Loaders/MTLLoader.js';
+import { OBJLoader } from '../Loaders/OBJLoader.js';
 
+function loadCar(scene, carType, mtlPath, objPath) {
+  if (!scene.userData) {
+    scene.userData = {};
+  }
 
-export function loadnormalcar(scene){
+  const mtlLoader = new MTLLoader();
+  mtlLoader.load(mtlPath, function (materials) {
+    materials.preload();
 
-     
-    const mtlLoader = new MTLLoader();
-    mtlLoader.load('./gcw/carros/carro_normal.mtl',
-      function(materials){
-        materials.preload();
- 
-           const loader = new OBJLoader();
-           loader.setMaterials(materials);
-            loader.load(
-                './gcw/carros/carro_normal.obj', // Ruta de tu archivo OBJ
-                function (object) {
-              //    handleObjectMovement(object, { left: 37, right: 39, up: 38, down: 40  });
-                 object.scale.set(.05,.05,.05);
-                  object.position.set(0,5,0);
-                    scene.add(object);
-                },
-                function (xhr) {
-                    console.log((xhr.loaded / xhr.total * 100) + '% cargado');
-                },
-                function (error) {
-                    console.error('Ocurrió un error al cargar el modelo: ', error);
-                }
-            );
-      }
- 
- 
- 
-  );
+    const loader = new OBJLoader();
+    loader.setMaterials(materials);
+    loader.load(objPath, function (object) {
+      object.scale.set(0.05, 0.05, 0.05);
+      object.position.set(0, 5, 0);
+      scene.add(object);
 
+      // Asignar el objeto al userData correspondiente
+      scene.userData[carType] = object;
+    },
+    function (xhr) {
+      console.log((xhr.loaded / xhr.total * 100) + '% cargado');
+    },
+    function (error) {
+      console.error('Ocurrió un error al cargar el modelo: ', error);
+    });
+  });
 }
 
-
-export function loadconvertiblecar(scene){
-
-        // Cargar el modelo OBJ
-        const mtlLoader2 = new MTLLoader();
-        mtlLoader2.load('./gcw/carros/convertible.mtl',
-        function(materials){
-          materials.preload();
-   
-             const loader = new OBJLoader();
-             loader.setMaterials(materials);
-              loader.load(
-                  './gcw/carros/convertible.obj', // Ruta de tu archivo OBJ
-                  function (object) {
-                   
-            //       handleObjectMovement2(object, { left: 65, right: 68, up: 87, down: 83 });
-                   object.scale.set(.05,.05,.05);
-                    object.position.set(5,0,0);
-                      scene.add(object);
-                  },
-                  function (xhr) {
-                      console.log((xhr.loaded / xhr.total * 100) + '% cargado');
-                  },
-                  function (error) {
-                      console.error('Ocurrió un error al cargar el modelo: ', error);
-                  }
-              );
-        }
-   
-   
-   
-    );
-
+export function loadnormalcar(scene) {
+  loadCar(scene, 'normalCar', './gcw/carros/carro_normal.mtl', './gcw/carros/carro_normal.obj');
 }
 
-
-export function loadeportivecar(scene){
-    
-          // Cargar el modelo OBJ
-        const mtlLoader3 = new MTLLoader();
-        mtlLoader3.load('./gcw/carros/deportivo.mtl',
-        function(materials){
-          materials.preload();
-   
-             const loader = new OBJLoader();
-             loader.setMaterials(materials);
-              loader.load(
-                  './gcw/carros/deportivo.obj', // Ruta de tu archivo OBJ
-                  function (object) {
-   
-                   object.scale.set(.05,.05,.05);
-                    object.position.set(-10,0,0);
-                      scene.add(object);
-                  },
-                  function (xhr) {
-                      console.log((xhr.loaded / xhr.total * 100) + '% cargado');
-                  },
-                  function (error) {
-                      console.error('Ocurrió un error al cargar el modelo: ', error);
-                  }
-              );
-        }
-   
-   
-   
-    );
-
+export function loadmusclecar(scene) {
+  loadCar(scene, 'muscleCar', './gcw/carros/muscle.mtl', './gcw/carros/muscle.obj');
 }
 
-
-export function loadlambocar(scene){
-    
-               // Cargar el modelo OBJ
-        const mtlLoader4 = new MTLLoader();
-    mtlLoader4.load('./gcw/carros/lambo.mtl',
-        function(materials){
-          materials.preload();
-   
-             const loader = new OBJLoader();
-             loader.setMaterials(materials);
-              loader.load(
-                  './gcw/carros/lambo.obj', // Ruta de tu archivo OBJ
-                  function (object) {
-   
-                   object.scale.set(.05,.05,.05);
-                    object.position.set(-20,0,0);
-                      scene.add(object);
-                  },
-                  function (xhr) {
-                      console.log((xhr.loaded / xhr.total * 100) + '% cargado');
-                  },
-                  function (error) {
-                      console.error('Ocurrió un error al cargar el modelo: ', error);
-                  }
-              );
-        }
-   
-   
-   
-    );
+export function loadeportivecar(scene) {
+  loadCar(scene, 'deportivoCar', './gcw/carros/deportivo.mtl', './gcw/carros/deportivo.obj');
 }
 
+export function loadconvertiblecar(scene) {
+  loadCar(scene, 'convertibleCar', './gcw/carros/convertible.mtl', './gcw/carros/convertible.obj');
+}
 
-export function loadmusclecar(scene){
-                  // Cargar el modelo OBJ
-                  const mtlLoader5 = new MTLLoader();
-                  mtlLoader5.load('./gcw/carros/muscle.mtl',
-                  function(materials){
-             materials.preload();
-      
-                const loader = new OBJLoader();
-                loader.setMaterials(materials);
-                 loader.load(
-                     './gcw/carros/muscle.obj', // Ruta de tu archivo OBJ
-                     function (object) {
-      
-                      object.scale.set(.05,.05,.05);
-                       object.position.set(10,0,0);
-                         scene.add(object);
-                     },
-                     function (xhr) {
-                         console.log((xhr.loaded / xhr.total * 100) + '% cargado');
-                     },
-                     function (error) {
-                         console.error('Ocurrió un error al cargar el modelo: ', error);
-                     }
-                 );
-           }
-      
-      
-      
-       );
+export function loadlambocar(scene) {
+  loadCar(scene, 'lamboCar', './gcw/carros/lambo.mtl', './gcw/carros/lambo.obj');
 }
--- Class/Fonts.js
+++ Class/Fonts.js
@@ -1,11 +1,12 @@
 import {TextGeometry} from "../Loaders/TextGeometry.js";  
 import {FontLoader} from "../Loaders/FontLoader.js";
+import * as THREE from "../three.module.js";
 
 export function texthola(scene){
     
     const fontLoader = new FontLoader();
     fontLoader.load(
-    'Fonts/gentilis_regular.typeface.json',
+    '../Fonts/gentilis_regular.typeface.json',
     (font)=>{
         const textGeometry = new TextGeometry('Hola mundo',{
             height:2,
@@ -16,6 +17,31 @@ export function texthola(scene){
         const textMaterial = new THREE.MeshNormalMaterial();
         const textMesh = new THREE.Mesh(textGeometry,textMaterial);
         textMesh.position.set(0,40,0);
+        scene.add(textMesh);
+    }
+
+
+
+
+    );
+
+}
+
+export function textselect(scene){
+    
+    const fontLoader = new FontLoader();
+    fontLoader.load(
+    '../Fonts/gentilis_regular.typeface.json',
+    (font)=>{
+        const textGeometry = new TextGeometry('Escoge tu vehiculo',{
+            height:2,
+            size:4,
+            font: font,
+
+        });
+        const textMaterial = new THREE.MeshNormalMaterial();
+        const textMesh = new THREE.Mesh(textGeometry,textMaterial);
+        textMesh.position.set(-20,10,0);
         scene.add(textMesh);
     }
 
